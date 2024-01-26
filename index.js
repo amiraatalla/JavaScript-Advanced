f1931b8f52a4bf8492c5a57e85e21233cfec0ebf
amiraatalla <amiraatalla63@gmail.com>
2024-01-26 15:51:15 +0200

    ..

---
+++ index.js
@@ -0,0 +1,25 @@
+let age = 15;
+// console.log('====================================');
+// console.log(age);
+// console.log('====================================');
+
+
+// function
+
+function add(number1, number2, number3) {
+    sum = number1 + number2 + number3; //local variable
+    console.log(age);
+    console.log(sum);
+    return sum;
+}
+
+
+// const result = add(7, 8, 5); //global variable
+// console.log(result);
+console.log('******************');
+console.log(add(7, 8, 5));
+console.log('******************');
+
+function sub(number1, number2){
+
+}
